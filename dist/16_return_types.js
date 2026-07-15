0ad164a1631b06bcd3ec24e13598fab8a5eb222a
Midhunkumar-se <midhu025@gmail.com>
2026-07-15 05:39:34 +0530

    learned return types

---
+++ dist/16_return_types.js
@@ -0,0 +1,6 @@
+// inference example
+const doubleFunc = (n) => n * 2;
+//explicit return for exported/public functions
+export function toTitle(s) {
+    return `Hello ${s}`;
+}
---
+++ src/16_return_types.ts
@@ -0,0 +1,7 @@
+// inference example
+const doubleFunc = (n: number) => n * 2;
+
+//explicit return for exported/public functions
+export function toTitle(s: string): string {
+  return `Hello ${s}`;
+}
