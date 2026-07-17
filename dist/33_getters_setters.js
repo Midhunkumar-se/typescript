c0168fb1e28203130433ba78638970f9cef3cc7a
Midhunkumar-se <midhu025@gmail.com>
2026-07-17 08:59:53 +0530

    learned getters nad setters

---
+++ dist/33_getters_setters.js
@@ -0,0 +1,24 @@
+"use strict";
+class Temperature {
+    #c = 0;
+    constructor(celsius) {
+        this.celsius = celsius;
+    }
+    get celsius() {
+        return this.#c;
+    }
+    set celsius(value) {
+        if (Number.isNaN(value)) {
+            throw new Error("Not a number! please check input");
+        }
+        this.#c = value;
+    }
+    get fahrenheit() {
+        return (this.#c * 9) / 5 + 32;
+    }
+    set fahrenheit(f) {
+        this.celsius = ((f - 32) * 5) / 9;
+    }
+}
+const t = new Temperature(25);
+t.fahrenheit = 212;
---
+++ src/33_getters_setters.ts
@@ -0,0 +1,29 @@
+class Temperature {
+  #c = 0;
+
+  constructor(celsius: number) {
+    this.celsius = celsius;
+  }
+
+  public get celsius(): number {
+    return this.#c;
+  }
+
+  public set celsius(value: number) {
+    if (Number.isNaN(value)) {
+      throw new Error("Not a number! please check input");
+    }
+    this.#c = value;
+  }
+
+  get fahrenheit(): number {
+    return (this.#c * 9) / 5 + 32;
+  }
+
+  public set fahrenheit(f: number) {
+    this.celsius = ((f - 32) * 5) / 9;
+  }
+}
+
+const t = new Temperature(25);
+t.fahrenheit = 212;
